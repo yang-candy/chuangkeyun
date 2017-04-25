180d0bd3f66dab73a604a8b2604bd94791f552e0
chenhui <1114545064@qq.com>
2017-04-25 16:55:22 +0800

    新增文章

---
+++ src/cgc/js/unit.js
@@ -0,0 +1,33 @@
+"use strict";
+var vm = {
+
+  //判断机型
+  mobileType: function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/windows phone/i.test(userAgent)) {
+      return "Windows Phone";
+    }
+    if (/android/i.test(userAgent)) {
+      return "Android";
+    }
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
+    }
+    return "unknown";
+  },
+
+  //正则去除空格
+  trim: function(str) {
+    return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+  },
+
+  //获取url
+  getParam: function gup(name, url) {
+    if (!url) url = location.href;
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(url);
+    return results == null ? null : results[1];
+  }
+};
